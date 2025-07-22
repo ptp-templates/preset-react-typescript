eacec07d4f4d638f581789ef24ec9fee8ed5b517
yohan <yhl0078@naver.com>
2025-07-23 00:46:23 +0900

    err test

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const test = 'test';
+  console.log(test);
   return (
     <div className="App">
       <header className="App-header">
